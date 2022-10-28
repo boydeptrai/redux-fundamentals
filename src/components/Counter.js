8d8b816fce3e48927baafc19437dd7e2de7eb1d7
Tuan Tran <langthamht047@gmail.com>
2022-10-28 02:38:09 -0700

    binding actions

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import {useSelector,useDispatch} from 'react-redux'
+import { bindActionCreators } from 'redux';
 import { decrement, increment, set } from '../store/action';
 
 export default function Counter() {
     const incident = "Incident";
     const count = useSelector((state) =>state.count);
     const dispatch = useDispatch();
+
+    const actions = bindActionCreators({increment,decrement,set},dispatch)
   return (
     <main className="counter">
       <h1>Days Since Last {incident}</h1>
       <p className="count">{count}</p>
       <section className="controls">
-        <button onClick={() =>dispatch(increment())}>Increment</button>
-        <button onClick={() =>dispatch(set(0))}>Reset</button>
-        <button onClick={() =>dispatch(decrement())}>Decrement</button>
+        <button onClick={() =>actions.increment()}>Increment</button>
+        <button onClick={() =>actions.set(0)}>Reset</button>
+        <button onClick={() =>actions.decrement()}>Decrement</button>
       </section>
     </main>
   )
--- src/components/SetCounter.js
+++ src/components/SetCounter.js
@@ -1,13 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { set } from '../store/action';
 
 export default function SetCounter() {
+  const countFromStore = useSelector((state) =>state.count);
+  const [count, setCount] = useState(countFromStore);
+  const dispatch = useDispatch();
+
+  useEffect(() =>{
+    setCount(countFromStore)
+  },[countFromStore])
   return (
     <section className="controls">
-        <form onSubmit={(event) =>{event.preventDefault()}}>
-            <label htmlFor="set-to">Set Count</label>
-            <input type="number" id="set-to" />
-            <input type="submit" />
-        </form>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          dispatch(set(count));
+        }}
+      >
+        <label htmlFor="set-to">Set Count</label>
+        <input
+          type="number"
+          id="set-to"
+          value={count}
+          onChange={(event) => setCount(event.target.value)}
+        />
+        <input type="submit" />
+      </form>
     </section>
-  )
+  );
 }
